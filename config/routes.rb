afd51191dcc68650446b0bfe3e4953eadc5b0d42
Stan <solve153@gmail.com>
2017-06-10 16:16:53 +0800

    Implement signup/login/logout api

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
+      post "/signup" => "auth#signup"
+      post "/login" => "auth#login"
+      post "/logout" => "auth#logout"
+
       resources :trains, :only => [:index, :show]
       resources :reservations, :ony => [:index, :show, :create, :update, :destroy]
     end
