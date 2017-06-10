aabef0eb6b5a27826b7959aacc1824d854b7f7da
Stan <solve153@gmail.com>
2017-06-10 15:47:48 +0800

    update model Train & availale_seats method

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,8 +1,20 @@
 class Train < ApplicationRecord
   validates_presence_of :number
-    has_many :reservations
+  has_many :reservations
 
-    def available_seats
-      ["1A", "1B", "1C", "1D", "1F"]
+  # 產生所有位置從 1A~6C
+  # ["1A", "1B", "1C", "2A", "2B", "2C", "3A", "3B", "3C",
+  #  "4A", "4B", "4C", "5A", "5B", "5C", "6A", "6B", "6C"]
+  SEATS = begin
+    (1..6).to_a.map do |series|
+      ["A","B","C"].map do |letter|
+        "#{series}#{letter}"
+      end
     end
+  end.flatten
+
+  def available_seats
+    # 所有 SEATS 扣掉已經訂位的資料
+    return SEATS - self.reservations.pluck(:seat_number)
+  end
 end
