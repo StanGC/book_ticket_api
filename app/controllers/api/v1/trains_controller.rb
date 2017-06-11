3076d1c4507858157fd1c6440832cfe5a1cbd9ee
Stan <solve153@gmail.com>
2017-06-11 10:31:41 +0800

    api/v1/trains#index use Jbuilder

--- app/controllers/api/v1/trains_controller.rb
+++ app/controllers/api/v1/trains_controller.rb
@@ -2,14 +2,8 @@ class Api::V1::TrainsController < ApiController
 
   def index
     @trains = Train.all
-    render :json => {
-      :data => @trains.map{ |train|
-        { :number => train.number,
-          :train_url => api_v1_train_url(train.number)
-          }
-      }
-    }
   end
+
   def show
     @train = Train.find_by_number!( params[:train_number] )
   end
